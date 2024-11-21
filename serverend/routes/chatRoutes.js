25f833b2de406fbd23c5094b030e7c035792878f
Developer242424 <developer@fefdypartners.com>
2024-11-21 15:40:56 +0530

    Delete serverend directory
    no need

--- serverend/routes/chatRoutes.js
+++
@@ -1,8 +0,0 @@
-import express, { Router } from "express"
-import { open } from "../controllers/chatController.js"
-
-const router = express.Router()
-
-router.route('/').get(open)
-
-export default router
